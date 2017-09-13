e66acc7f6d8951826839357a54426a3a852f8136
Nick <kiwi.sedna@gmail.com>
2017-09-14 00:01:04 +1200

    render list

--- src/containers/main.js
+++ src/containers/main.js
@@ -1,23 +1,67 @@
 import React, { Component } from 'react';
-import { Button } from 'react-native';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Container, List, ListItem, Text } from 'native-base';
+import styled from 'styled-components/native';
 
+import * as locationsActions from '../actions/locations';
+
+
+const StyledContainer = styled(Container)`
+  background-color: white;
+`;
 
 class MainScreen extends Component {
-  static navigationOptions = {
-    title: 'Welcome',
-  };
+  constructor(props) {
+    super(props);
+    this.onLocationItemPress = this.onLocationItemPress.bind(this);
+  }
+
+  componentDidMount() {
+    // TODO: https://facebook.github.io/react-native/docs/geolocation.html
+    const pos = {
+      coords: {
+        latitude: -36.92507,
+        longitude: 174.73578,
+      },
+    };
+
+    this.props.actions.fetchLocations(pos);
+  }
+
+  onLocationItemPress() {
+  }
 
   render() {
-    const { navigate } = this.props.navigation;
+    const { items } = this.props.locations;
+
     return (
-      <Button
-        title="Go to Jane's profile"
-        onPress={() =>
-          navigate('Profile', { name: 'Jane' })
-        }
-      />
+      <StyledContainer>
+        <List>
+          {items.map(item => (
+            <ListItem key={item.id} onPress={this.onLocationItemPress}>
+              <Text>{item.name}</Text>
+            </ListItem>
+          ))}
+        </List>
+      </StyledContainer>
     );
   }
 }
 
-export default MainScreen;
+MainScreen.navigationOptions = {
+  title: 'Nearby',
+};
+
+const mapStateToProps = ({ locations }) => {
+  return { locations };
+};
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(locationsActions, dispatch),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(MainScreen);
