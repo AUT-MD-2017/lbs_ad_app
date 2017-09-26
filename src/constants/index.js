c69a1173fe15242018c15f283b72ffa200e96204
Nick <kiwi.sedna@gmail.com>
2017-09-26 17:59:32 +1300

    replace native-base components with the official ones in react-native

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,10 @@
 export const API_ADDRESS = `http://${
   __DEV__ ? '0.0.0.0:3000' : 'yourhost.com'
 }/api/`;
+
+export const WHITE = '#fff';
+export const BLACK = '#333';
+export const DARK_GREY = '#666';
+export const GREY = '#999';
+export const LIGHT_GREY = '#eee';
+export const LIGHT_WHITE = '#f9f9f9';
--- src/containers/nearby.js
+++ src/containers/nearby.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { List, ListItem, Text } from 'native-base';
+import {
+  ListView, TouchableHighlight, Text, View,
+} from 'react-native';
+import styled from 'styled-components/native';
 
+import * as consts from '../constants';
 import { Container } from '../components/misc';
 import * as locationsActions from '../actions/locations';
 
+
+const Item = styled(View)`
+  background-color: ${consts.WHITE};
+  align-items: center;
+  flex-direction: row;
+  height: 60;
+  paddingHorizontal: 24;
+  border-bottom-color: ${consts.LIGHT_GREY};
+  border-bottom-width: 1;
+`;
+
+const LeftView = styled(View)`
+  flex: 1;
+  flex-direction: column;
+`;
+
+const RightView = styled(View)`
+`;
+
+const LocationName = styled(Text)`
+  font-size: 16;
+  color: ${consts.BLACK};
+  margin-bottom: 5;
+`;
+
+const Category = styled(Text)`
+  color: ${consts.GREY};
+`;
+
+const Distance = styled(Text)`
+  margin-left: 10;
+`;
 
 class NearbyScreen extends Component {
   static navigationOptions = {
@@ -29,23 +65,37 @@ class NearbyScreen extends Component {
     navigate('Location', { location });
   }
 
+  renderRow = rowData => (
+    <TouchableHighlight
+      onPress={() => this.onLocationItemPress(rowData)}
+    >
+      <Item>
+        <LeftView>
+          <LocationName>{rowData.name}</LocationName>
+          <Category>{rowData.category}</Category>
+        </LeftView>
+        <RightView>
+          <Distance>{rowData.distance}</Distance>
+        </RightView>
+      </Item>
+    </TouchableHighlight>
+  );
+
   render() {
     const { locations: { items } } = this.props;
 
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1.id !== r2.id,
+    });
+    const dataSource = ds.cloneWithRows(items);
+
     return (
       <Container>
-        <List>
-          {items.map(item => (
-            <ListItem
-              key={item.id}
-              onPress={() => {
-                this.onLocationItemPress(item);
-              }}
-            >
-              <Text>{item.name}</Text>
-            </ListItem>
-          ))}
-        </List>
+        <ListView
+          enableEmptySections
+          dataSource={dataSource}
+          renderRow={this.renderRow}
+        />
       </Container>
     );
   }
